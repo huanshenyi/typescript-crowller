23111a037564f17ca12ca45cab0488fe476564b8
huanshenyi <txy1226052@gmail.com>
2020-01-18 14:36:09 +0900

    データ取得完了

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ tsc --init
 npm install ts-node -D
 npm install typescript -D
 
+# ライトのhttpライブラリ
 npm install superagent --save
 npm install @types/superagent -D
+```
+
+# html解析ライブラリ
+```
+npm install cheerio --save
+npm install @types/cheerio
 ```
--- src/crowller.ts
+++ src/crowller.ts
@@ -1,14 +1,35 @@
 // ts ->.d.ts(翻訳ファイア) -> js
 import superagent from "superagent";
+import cheerio from "cheerio"
+
+interface Course {
+    title: string, 
+    count: number
+}
 
 class Crowller {
     private secret = "secretKey";
     url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-    private rawHtml = "";
+
+    getCourseInfo(html:string){
+      const $ = cheerio.load(html)
+      const courseItems = $(".course-item");
+      const courseInfos: Course[] = [];
+      courseItems.map((index, element)=>{
+          const desc = $(element).find(".course-desc");
+          const title = desc.eq(0).text()
+          const count:number = parseInt(desc.eq(1).text().split("：")[1], 10)
+          courseInfos.push({title, count})
+      });
+      const result = {
+          time:(new Date()).getTime(),
+          data :courseInfos
+      };
+    }
 
     async getRawHtml(){
        const result = await superagent.get(this.url);
-       this.rawHtml = result.text
+       this.getCourseInfo(result.text);
     }
     constructor() {
         this.getRawHtml();
