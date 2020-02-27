b1d177e27b18b3a1ade77cd2ac05cfd82178ad79
huanshenyi <txy1226052@gmail.com>
2020-02-28 00:06:14 +0900

    logout showData getData関連のapi追加nodeファイア操作も追加

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import {Router, Request, Response} from "express";
 import Crowller from './crowller'
 import DellAnalyzer from "./dellAnalyzer"
+import fs from "fs"
+import path from "path"
 
 interface RequestWithBody extends Request{
   body: {
@@ -10,22 +12,42 @@ interface RequestWithBody extends Request{
 
 const router = Router();
 router.get('/', (req:Request, res:Response) => {
-    res.send(`
-    <html>
-      <body>
-        <form method="post" action="/login">
-          <input type="password" name="password" />
-          <button>ログイン</button>
-        </form>
-      </body>
-    </html>
-    `)
+    const isLogin = req.session ? req.session.login : false;
+    if (isLogin){
+       res.send(`
+       <html>
+          <body>
+             <div>home</div>
+             <a href="/getData">データ取得</a>
+             <a href="/showData">データ表示</a>
+             <a href="/logout">ログアウト</a>
+          </body>
+       </html>
+       `)  
+    }else{
+      res.send(`
+      <html>
+        <body>
+          <form method="post" action="/login">
+            <input type="password" name="password" />
+            <button>ログイン</button>
+          </form>
+        </body>
+      </html>
+      `)
+    }
  })
- 
+
+router.get('/logout', (req:RequestWithBody, res:Response)=>{
+   if(req.session){
+    req.session.login = undefined;
+   }
+   res.redirect('/');
+}) 
+
 router.post('/login', (req:RequestWithBody, res:Response) => {
   const {password} = req.body;
   const isLogin = req.session ? req.session.login : false;
-
   if(isLogin) {
     res.send('logined')
   }else {
@@ -38,18 +60,33 @@ router.post('/login', (req:RequestWithBody, res:Response) => {
   }
 })
 
-router.post('/getData', (req:RequestWithBody, res:Response)=>{
-    const {password} = req.body;
-    if(req.body.password === '123'){
-        const { password } = req.body 
+router.get('/getData', (req:RequestWithBody, res:Response)=>{
+    const isLogin = req.session ? req.session.login : false;
+    if(isLogin){ 
         const secret = "secretKey";
         const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
         const analyzer = DellAnalyzer.getInstance();
         new Crowller(url, analyzer);
         res.send('getData Success!')
     }else {
-        res.send(`password Error`)
+        res.redirect('/login')
     }
+})
+
+router.get("/showData", (req:RequestWithBody, res:Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if(isLogin){
+    try{
+      const position = path.resolve(__dirname, "../data/course.json");
+      const result = fs.readFileSync(position, "utf-8");
+      res.json(JSON.parse(result));
+    }catch(e){
+       res.send("また内容ありません")
+    }
+  }else{
+    res.redirect("/");
+  }
+
 })
 
  export default router;
